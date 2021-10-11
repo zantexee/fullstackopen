04e8438e6d727dcf74e49193ddb73fcac9ffc321
Flowertin Madalin <60981000+zantexee@users.noreply.github.com>
2021-10-11 09:39:27 +0300

    countries 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -12,11 +12,10 @@ const App = () => {
   }, []);
 
   const filteredCountries = countries.filter((currCountry) => {
-    console.log(currCountry);
     return currCountry.name.common.toLowerCase().includes(filter.toLowerCase());
   });
+
   const handleFilterChange = (event) => {
-    console.log(filter);
     setFilter(event.target.value);
   };
 
@@ -26,9 +25,33 @@ const App = () => {
         find countries <input value={filter} onChange={handleFilterChange} />
       </div>
       <div>
-        {filteredCountries.length > 10
+        {filteredCountries.length > 10 && filteredCountries.length !== 1
           ? 'Too many matches, specify another filter'
-          : filteredCountries.map((currCountry) => <p>{currCountry.name.common}</p>)}
+          : filteredCountries.map((currCountry) => {
+              if (filteredCountries.length > 1) return <p>{currCountry.name.common}</p>;
+              else return null;
+            })}
+        {filteredCountries.length === 1 && (
+          <div>
+            <h1>{filteredCountries[0].name.common}</h1>
+            <p>
+              capital {filteredCountries[0].capital} <br />
+              population {filteredCountries[0].population}
+            </p>
+            <h2>languages</h2>
+            <ul>
+              {Object.values(filteredCountries[0].languages).map((currLanguage) => (
+                <li key={`${filteredCountries[0].name.common}`}>{currLanguage}</li>
+              ))}
+            </ul>
+            <img
+              src={`${filteredCountries[0].flags.svg}`}
+              alt="country flag"
+              width="150px"
+              height="150px"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
